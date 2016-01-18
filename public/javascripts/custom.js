62abb57226e3a507999eabad10e210b78b4b0e61
Uziel Valdez <uzielvaldez85@gmail.com>
2016-01-18 15:35:44 -0600

    video

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,38 +1,37 @@
 (function () {
-  var argImages = ['bg.jpg','bg1.jpg'];
 	/* process to subscribe to the keydown event in the roomName input in order 
 	to let the user know if it has rich the max length for a room name */
-  var roomName = document.querySelector('#roomName'),//room name element
+  var roomName = document.querySelector( "#roomName" ),//room name element
   roomNameMaxLength = 12,//max length for a room name
-  lengthElem = document.querySelector('#length'),//length label element
-  btnStart = document.querySelector("#start");
+  lengthElem = document.querySelector( "#length" ),//length label element
+  btnStart = document.querySelector( "#start" );
 
-  //first an observable is added for the keydown event in the roomName element
-  roomNameKeydownStream = Rx.Observable.fromEvent(roomName,'keydown')
+  // first an observable is added for the keydown event in the roomName element
+  roomNameKeydownStream = Rx.Observable.fromEvent( roomName, "keydown" )
   	.map(function(e){
-  		if(e.keyCode == 8 || e.keyCode == 46)//if user press backspace or delete key it will update the label  		
-  			return (roomNameMaxLength - e.target.value.length)+1;
-  		if(e.target.value.length >= roomNameMaxLength)//if length value has rich max length allowed we stop the user from entering more characters
+  		if( e.keyCode == 8 || e.keyCode == 46 )// if user press backspace or delete key it will update the label  		
+  			return ( roomNameMaxLength - e.target.value.length ) + 1;
+  		if( e.target.value.length >= roomNameMaxLength )//if length value has rich max length allowed we stop the user from entering more characters
   			e.preventDefault();
       if( e.keyCode === 13 ){//if user hit enter it will redirect to start the meeting
-        window.location.href = "/start/"+e.target.value;
+        window.location.href = "/start/" + e.target.value;
       }      
   		return roomNameMaxLength - e.target.value.length;
   	})
 	.distinctUntilChanged();
 
-	//function to update the html element
-  	function setHTML(value){
-  		lengthElem.innerHTML=value;
+	//function to update the html element, room name
+  	function setHTML( value ){
+  		lengthElem.innerHTML = value;
   	}
 
   	//we bind the function to the stream
-  roomNameKeydownStream.subscribe(setHTML.bind());
+  roomNameKeydownStream.subscribe( setHTML.bind() );
 
-  btnStartClickStream = Rx.Observable.fromEvent(btnStart,"click");
+  btnStartClickStream = Rx.Observable.fromEvent( btnStart, "click" );
   btnStartClickStream.subscribe(function(){      
-      if(roomName.value.length){
-        window.location.href = "/start/"+roomName.value;
+      if( roomName.value.length ){
+        window.location.href = "/start/" + roomName.value;
       }
   });
 
--- public/javascripts/opentok.variables.js
+++ public/javascripts/opentok.variables.js
@@ -1,28 +1,35 @@
 var apiKey = '45462192',
-	sessionId = document.querySelector("#opentokSessionId"),
-	token = document.querySelector("#opentokToken"),
-	session = OT.initSession(apiKey, sessionId.value);
-$(".publisher").css({"width":"100%"});
-$(".loading").show();
+	sessionId = document.querySelector( "#opentokSessionId" ),
+	token = document.querySelector( "#opentokToken" ),
+	session = OT.initSession( apiKey, sessionId.value );
+
+// we start with publisher covering all screen
+$( ".publisher" ).css( {"width": "100%"} );
+$( ".loading" ).show();
+
 session.on({ 
-  streamCreated: function(event) {
-    if ( $("#subscribersDiv").find(".OT_subscriber").length == 0){
-	    session.subscribe(event.stream, 'subscribersDiv', {width: "100%", height: 400, insertMode: 'append'});
-	    $(".publisher").css({"width":"49%"}); 
-	    $("#subscribersDiv").css({"display":"inline-block"});
+  streamCreated: function( event ) {
+  	// if there is not any subscriber already we add new stream
+    if ( $( "#subscribersDiv" ).find( ".OT_subscriber" ).length == 0 ){
+	    session.subscribe( event.stream, 'subscribersDiv', 
+	    	{ width: "100%", height: 400, insertMode: 'append'} );
+	    $( ".publisher" ).css( {"width": "49%"} ); 
+	    $( "#subscribersDiv" ).css( {"display": "inline-block"} );
     }
   },
   streamDestroyed: function(event){
-	$("#subscribersDiv").css({"display":"none"});
-	$(".publisher").css({"width":"100%"});
+  	// if user leaves the chat room, publisher will cover all screen again
+	$( "#subscribersDiv" ).css( {"display": "none"} );
+	$(".publisher" ).css( {"width":"100%"} );
   }
 });
-/*session.connect(token.value, function(error) {
-	if (error) {
-	  console.log(error.message);
+
+session.connect(token.value, function(error) {
+	if ( error ) {
+	  console.log( error.message );
 	} else {
-		$(".publisher").css({"display":"inline-block"});
-		$(".loading").hide();
-	  	session.publish('myPublisherDiv', {width: "100%", height: 400}); 
+		$( ".publisher" ).css( {"display": "inline-block"} );
+		$( ".loading" ).hide();
+	  	session.publish( 'myPublisherDiv', {width: "100%", height: 400} ); 
 	}
-});*/
+});
