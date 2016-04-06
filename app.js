ce835c29b0c9b5a2a507e5c7648838dc5c2462f5
Uziel Valdez <uzielvaldez85@gmail.com>
2016-04-05 22:27:47 -0700

    npm version added to package.json

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.use(function(err, req, res, next) {
   });
 });
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000!');
 });
 module.exports = app;
